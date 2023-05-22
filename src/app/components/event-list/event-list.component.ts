55d2adad894b38cdd273b845994a5600bec1793c
NataliSemi <nata654s@yahoo.com>
2023-05-22 16:13:36 +0100

    build

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, AfterViewInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
+
 import { NasaService } from '../../services/nasa.service';
 import { Router } from '@angular/router';
 
@@ -7,11 +8,37 @@ import { Router } from '@angular/router';
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.css']
 })
-export class EventListComponent implements OnInit {
+export class EventListComponent implements OnInit, AfterViewInit {
+  @ViewChild('spaceship', { static: false }) spaceshipRef?: ElementRef;
   events!: any[];
 
-  constructor(private nasaService: NasaService, private router: Router) {}
+  constructor(
+    private nasaService: NasaService,
+    private router: Router,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const spaceship = this.spaceshipRef?.nativeElement as HTMLElement;
+      if (spaceship) {
+        const displayWidth = window.innerWidth;
+        const displayHeight = window.innerHeight;
+
+        const getRandomPosition = () => {
+          const x = Math.random() * (displayWidth - spaceship.offsetWidth);
+          const y = Math.random() * (displayHeight - spaceship.offsetHeight);
+          this.renderer.setStyle(spaceship, 'left', x + 'px');
+          this.renderer.setStyle(spaceship, 'top', y + 'px');
+        };
+
+        getRandomPosition();
+        setInterval(getRandomPosition, 3000); // Change the interval value as desired
+      }
+    }, 100);
+  }
+  
   ngOnInit(): void {
     this.getNasaEvents();
   }
@@ -25,6 +52,7 @@ export class EventListComponent implements OnInit {
       // Perform some action when the user scrolls beyond a certain point
     }
   }
+  
   getNasaEvents(): void {
     this.nasaService.getNasaEvents().subscribe(
       (response) => {
