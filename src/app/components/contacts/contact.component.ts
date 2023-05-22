6df25119dd8f917f795b9dc011efba73f409333a
NataliSemi <nata654s@yahoo.com>
2023-05-22 18:16:11 +0100

    prod

--- src/app/components/contacts/contact.component.ts
+++ src/app/components/contacts/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 import { NasaImagesService } from '../../services/nasa-image-of-day.service';
 
 @Component({
@@ -6,19 +6,43 @@ import { NasaImagesService } from '../../services/nasa-image-of-day.service';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, AfterViewInit {
   imageOfDay: any;
 
-  constructor(private nasaImagesService: NasaImagesService) { }
+  constructor(
+    private nasaImagesService: NasaImagesService,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     this.getImageOfDay();
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const spaceship = this.elementRef.nativeElement.querySelector('.spaceship') as HTMLElement;
+      if (spaceship) {
+        const displayWidth = window.innerWidth;
+        const displayHeight = window.innerHeight;
+
+        const getRandomPosition = () => {
+          const x = Math.random() * (displayWidth - spaceship.offsetWidth);
+          const y = Math.random() * (displayHeight - spaceship.offsetHeight);
+          this.renderer.setStyle(spaceship, 'left', x + 'px');
+          this.renderer.setStyle(spaceship, 'top', y + 'px');
+        };
+
+        getRandomPosition();
+        setInterval(getRandomPosition, 13000); // Change the interval value as desired
+      }
+    }, 100);
+  }
+
   getImageOfDay(): void {
     this.nasaImagesService.getImageOfDay().subscribe(
       (response: any) => {
-        console.log(response)
+        console.log(response);
         this.imageOfDay = response;
       },
       (error) => {
--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, HostListener, OnInit, AfterViewInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
-
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NasaService } from '../../services/nasa.service';
 import { Router } from '@angular/router';
 
@@ -8,23 +7,13 @@ import { Router } from '@angular/router';
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.css']
 })
-export class EventListComponent implements OnInit, AfterViewInit {
-  @ViewChild('spaceship', { static: false }) spaceshipRef?: ElementRef;
+export class EventListComponent implements OnInit {
   events!: any[];
 
-  constructor(
-    private nasaService: NasaService,
-    private router: Router,
-    private elementRef: ElementRef,
-    private renderer: Renderer2
-  ) {}
+  constructor(private nasaService: NasaService, private router: Router) {}
 
-  ngAfterViewInit(): void {
-    this.getNasaEvents(); // Call the method to fetch NASA events
-  }
-  
   ngOnInit(): void {
-    // Initialize component
+    this.getNasaEvents();
   }
 
   @HostListener('window:wheel', ['$event'])
@@ -36,37 +25,15 @@ export class EventListComponent implements OnInit, AfterViewInit {
       // Perform some action when the user scrolls beyond a certain point
     }
   }
-  
   getNasaEvents(): void {
     this.nasaService.getNasaEvents().subscribe(
       (response) => {
         this.events = response.events;
-        this.positionSpaceship(); // Call the method to position the spaceship
       },
       (error) => {
         console.log('Error:', error);
       }
     );
-  }
-
-  positionSpaceship(): void {
-    setTimeout(() => {
-      const spaceship = this.spaceshipRef?.nativeElement as HTMLElement;
-      if (spaceship) {
-        const displayWidth = window.innerWidth;
-        const displayHeight = window.innerHeight;
-
-        const getRandomPosition = () => {
-          const x = Math.random() * (displayWidth - spaceship.offsetWidth);
-          const y = Math.random() * (displayHeight - spaceship.offsetHeight);
-          this.renderer.setStyle(spaceship, 'left', x + 'px');
-          this.renderer.setStyle(spaceship, 'top', y + 'px');
-        };
-
-        getRandomPosition();
-        setInterval(getRandomPosition, 3000); // Change the interval value as desired
-      }
-    }, 100);
   }
 
   goToEventDetails(eventId: string): void {
